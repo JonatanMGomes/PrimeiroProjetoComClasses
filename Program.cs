352f6230cc11e7c291a385e556fc9f2b37a37b77
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-03-21 22:46:25 -0300

    Subindo a program, git ignore e cs proj

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using PrimeiroProjetoComClasses.Exercicios.ProjetoLojaVirtualEletronicos;
+
+namespace PrimeiroProjetoComClasses{
+    public class Program{
+          static void Main(string[] args){
+            Usuario Usuario = new Usuario();
+            Usuario.Nome = Usuario.PedeNome();
+            Usuario.Email = Usuario.PedeEmail();
+            CarrinhoCompras Carrinho = new CarrinhoCompras(Usuario);
+            Carrinho.IniciarCompras();
+         }
+     }
+}
