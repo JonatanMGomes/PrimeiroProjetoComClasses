1f99895a3b797c2de8a48ecbe9486e058e0980e2
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-03-23 23:03:51 -0300

    Aprendendo novas funcionalidades da classe List

--- Program.cs
+++ Program.cs
@@ -17,12 +17,28 @@ namespace PrimeiroProjetoComClasses{
               strings.Add(contador.ToString());
             }
             Console.WriteLine("Finalizou");
+            //adicionar outra lista
             numeros.AddRange(numerosMaisDez);
-
+            //remover item
             numeros.Remove(5);
             numeros.RemoveAt(3);
             numeros.RemoveAll(numeroARemover => numeroARemover < 10 || numeroARemover > 14);
             numeros.RemoveRange(0, 5);
+            //buscar index
+            var indexString = strings.IndexOf("a");
+            var indexCinco = numeros.IndexOf(5);
+            //buscar na lista
+            var valorNaPosicaoTres = numeros.ElementAt(3);
+            var valorPar = numeros.Find(numero => numero % 2 == 0);
+            var valoresParesDois = numeros.FindAll(numero => numero % 2 == 0);
+            //verificação
+            var temQuinze = numeros.Any(numero => numero == 15);
+            var temQuatorze = numeros.Exists(numero => numero == 14);
+            var todosSaoPares = numeros.All(numero => numero % 2 == 0);
+            var textoComMaisDeCincoLetras = strings.All(numero => strings.Count > 5);
+            //ordenação
+            var ordenacaoDescrescente = numeros.OrderByDescending(x => x);
+            var ordenacaoCrescente = numeros.OrderBy(x => x);
          }
      }
 }
